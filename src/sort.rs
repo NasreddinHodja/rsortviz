14b9476db1946393f4ac198b6149792fac45b153
Tomás Bizet <tbizetde@gmail.com>
2023-12-12 00:34:26 -0300

    refactor

--- src/sort.rs
+++ src/sort.rs
@@ -29,36 +29,38 @@ pub struct SortResult {
     pub used_indices: Vec<usize>,
 }
 
-pub fn bubble_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
+fn send_message(tx: &Sender<Option<SortResult>>, values: &[usize], used_indices: &[usize]) {
     tx.send(Some(SortResult {
         values: values.to_vec(),
-        used_indices: vec![0],
+        used_indices: used_indices.to_vec(),
     }))
     .unwrap();
+}
 
+pub fn bubble_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
+    send_message(&tx, values, &[]);
     let len = values.len();
+    let mut swapped;
+
     for i in 0..len {
-        for j in 0..(len - i - 1) {
+        swapped = false;
+
+        for j in 0..len - i - 1 {
             if values[j] > values[j + 1] {
                 values.swap(j, j + 1);
-                tx.send(Some(SortResult {
-                    values: values.to_vec(),
-                    used_indices: vec![i, j, j + 1],
-                }))
-                .unwrap();
+                send_message(&tx, values, &[j, j + 1]);
             }
         }
-    }
 
+        if !swapped {
+            break;
+        }
+    }
     tx.send(None).unwrap();
 }
 
 pub fn insertion_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
-    tx.send(Some(SortResult {
-        values: values.to_vec(),
-        used_indices: vec![0],
-    }))
-    .unwrap();
+    send_message(&tx, values, &[]);
 
     let len = values.len();
     for i in 1..len {
@@ -66,11 +68,7 @@ pub fn insertion_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
 
         while j > 0 && values[j] < values[j - 1] {
             values.swap(j, j - 1);
-            tx.send(Some(SortResult {
-                values: values.to_vec(),
-                used_indices: vec![j, j - 1],
-            }))
-            .unwrap();
+            send_message(&tx, values, &[j, j - 1]);
             j -= 1;
         }
     }
@@ -79,11 +77,7 @@ pub fn insertion_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
 }
 
 pub fn selection_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
-    tx.send(Some(SortResult {
-        values: values.to_vec(),
-        used_indices: vec![0],
-    }))
-    .unwrap();
+    send_message(&tx, values, &[]);
     let len = values.len();
 
     for i in 0..len {
@@ -93,11 +87,7 @@ pub fn selection_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
             if values[j] < values[min_index] {
                 min_index = j;
             }
-            tx.send(Some(SortResult {
-                values: values.to_vec(),
-                used_indices: vec![i, j, min_index],
-            }))
-            .unwrap();
+            send_message(&tx, values, &[i, j, min_index]);
         }
 
         values.swap(i, min_index);
@@ -105,11 +95,7 @@ pub fn selection_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
 }
 
 pub fn merge_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
-    tx.send(Some(SortResult {
-        values: values.to_vec(),
-        used_indices: vec![0],
-    }))
-    .unwrap();
+    send_message(&tx, values, &[]);
 
     let len = values.len();
     let mut current_size = 1;
@@ -129,20 +115,12 @@ pub fn merge_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
             while i < left.len() && j < right.len() {
                 if left[i] < right[j] {
                     values[k] = left[i];
-                    tx.send(Some(SortResult {
-                        values: values.to_vec(),
-                        used_indices: vec![i + start, j + mid, k],
-                    }))
-                    .unwrap();
+                    send_message(&tx, values, &[i + start, j + mid, k]);
                     k += 1;
                     i += 1;
                 } else {
                     values[k] = right[j];
-                    tx.send(Some(SortResult {
-                        values: values.to_vec(),
-                        used_indices: vec![i + start, j + mid, k],
-                    }))
-                    .unwrap();
+                    send_message(&tx, values, &[i + start, j + mid, k]);
                     k += 1;
                     j += 1;
                 }
@@ -150,22 +128,14 @@ pub fn merge_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
 
             while i < left.len() {
                 values[k] = left[i];
-                tx.send(Some(SortResult {
-                    values: values.to_vec(),
-                    used_indices: vec![i + start, j + mid, k],
-                }))
-                .unwrap();
+                send_message(&tx, values, &[i + start, j + mid, k]);
                 k += 1;
                 i += 1;
             }
 
             while j < right.len() {
                 values[k] = right[j];
-                tx.send(Some(SortResult {
-                    values: values.to_vec(),
-                    used_indices: vec![i + start, j + mid, k],
-                }))
-                .unwrap();
+                send_message(&tx, values, &[i + start, j + mid, k]);
                 k += 1;
                 j += 1;
             }
@@ -180,11 +150,7 @@ pub fn merge_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
 }
 
 pub fn quicksort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
-    tx.send(Some(SortResult {
-        values: values.to_vec(),
-        used_indices: vec![],
-    }))
-    .unwrap();
+    send_message(&tx, values, &[]);
     let mut stack = vec![(0, values.len())];
 
     while let Some((lo, hi)) = stack.pop() {
@@ -199,11 +165,7 @@ pub fn quicksort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
 
         for j in lo..hi - 1 {
             if values[j] <= pivot {
-                tx.send(Some(SortResult {
-                    values: values.to_vec(),
-                    used_indices: vec![i, j, pivot_index],
-                }))
-                .unwrap();
+                send_message(&tx, values, &[i, j, pivot_index]);
                 values.swap(i, j);
                 i += 1;
             }
@@ -216,20 +178,13 @@ pub fn quicksort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
         stack.push((pivot_index + 1, hi));
         stack.push((lo, pivot_index));
     }
-    tx.send(Some(SortResult {
-        values: values.to_vec(),
-        used_indices: vec![],
-    }))
-    .unwrap();
+
+    send_message(&tx, values, &[]);
     tx.send(None).unwrap();
 }
 
 pub fn heap_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
-    tx.send(Some(SortResult {
-        values: values.to_vec(),
-        used_indices: vec![],
-    }))
-    .unwrap();
+    send_message(&tx, values, &[]);
 
     for i in (0..values.len() / 2).rev() {
         max_heapify(values, i, values.len(), &tx);
@@ -238,11 +193,7 @@ pub fn heap_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
     for end in (1..values.len()).rev() {
         values.swap(0, end);
         max_heapify(values, 0, end, &tx);
-        tx.send(Some(SortResult {
-            values: values.to_vec(),
-            used_indices: vec![end],
-        }))
-        .unwrap();
+        send_message(&tx, values, &[end]);
     }
 }
 
@@ -253,29 +204,17 @@ fn max_heapify(values: &mut [usize], i: usize, heap_size: usize, tx: &Sender<Opt
 
     if left < heap_size && values[left] > values[largest] {
         largest = left;
-        tx.send(Some(SortResult {
-            values: values.to_vec(),
-            used_indices: vec![left, right, largest],
-        }))
-        .unwrap();
+        send_message(&tx, values, &[left, right, largest]);
     }
 
     if right < heap_size && values[right] > values[largest] {
         largest = right;
-        tx.send(Some(SortResult {
-            values: values.to_vec(),
-            used_indices: vec![left, right, largest],
-        }))
-        .unwrap();
+        send_message(&tx, values, &[left, right, largest]);
     }
 
     if largest != i {
         values.swap(i, largest);
-        tx.send(Some(SortResult {
-            values: values.to_vec(),
-            used_indices: vec![left, right, largest],
-        }))
-        .unwrap();
+        send_message(&tx, values, &[left, right, largest]);
         max_heapify(values, largest, heap_size, &tx);
     }
 }
@@ -283,11 +222,7 @@ fn max_heapify(values: &mut [usize], i: usize, heap_size: usize, tx: &Sender<Opt
 pub fn shell_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
     let len = values.len();
     let mut gap = len / 2;
-    tx.send(Some(SortResult {
-        values: values.to_vec(),
-        used_indices: vec![gap],
-    }))
-    .unwrap();
+    send_message(&tx, values, &[gap]);
 
     while gap > 0 {
         for i in gap..len {
@@ -295,11 +230,7 @@ pub fn shell_sort(values: &mut [usize], tx: Sender<Option<SortResult>>) {
             while j >= gap && values[j - gap] > values[j] {
                 values.swap(j - gap, j);
                 j -= gap;
-                tx.send(Some(SortResult {
-                    values: values.to_vec(),
-                    used_indices: vec![gap, i, j],
-                }))
-                .unwrap();
+                send_message(&tx, values, &[gap, i, j]);
             }
         }
 
@@ -339,17 +270,9 @@ fn counting_sort(values: &mut [usize], exp: usize, tx: &Sender<Option<SortResult
         let index = (num / exp) % 10;
         output[count[index] - 1] = num;
         count[index] -= 1;
-        tx.send(Some(SortResult {
-            values: output.to_vec(),
-            used_indices: vec![index],
-        }))
-        .unwrap();
+        send_message(&tx, values, &[index]);
     }
 
     values.copy_from_slice(&output);
-    tx.send(Some(SortResult {
-        values: values.to_vec(),
-        used_indices: vec![],
-    }))
-    .unwrap();
+    send_message(&tx, values, &[]);
 }
